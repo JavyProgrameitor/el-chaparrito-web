3fc698e620a16498121748d2f299ed320c54b4ad
Programeitor <f.j.gg2@hotmail.com>
2026-01-20 20:31:16 +0100

    feat: :sparkles: Change letters for the more impact

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        // opcional: para títulos western y texto normal
+         display: ["Rye", "serif"],
+      },
       colors: {
         jd: {
           green: "#367C2B",
